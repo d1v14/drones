50b389d7687356d4c0e6333bca451714cf96824e
Dmitry Novikov <dmitriinov12@gmail.com>
2024-03-13 13:55:26 +0300

    delete

--- mappingCppSimple/build/mappingCpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmitry/drones/mappingCpp/src/mappingCpp
-# Build directory: /home/dmitry/drones/mappingCpp/build/mappingCpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
